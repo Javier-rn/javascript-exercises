ded4d8cb4e3dd8040d30556a68187ac9aa0cd42c
Javier <javier96rn@gmail.com>
2022-03-22 10:36:25 +0100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, nTimes) {
+    if (nTimes < 0) {
+        return 'ERROR';
+    }
+    let result = '';
+    for (let i = 0; i < nTimes; i++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
