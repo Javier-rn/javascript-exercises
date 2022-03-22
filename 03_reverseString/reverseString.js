74ba21623f3605fc58f193ef7c0150ac649c4be0
Javier <javier96rn@gmail.com>
2022-03-22 10:48:53 +0100

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    let result = '';
+    const characters = str.split('');
 
+    for (let i = characters.length - 1; i >= 0; i--) {
+        result += characters[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
