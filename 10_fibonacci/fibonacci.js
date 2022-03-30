1dc52e67877b4ff3d76988c4c6f1110d7e2fcfaa
Javier <javier96rn@gmail.com>
2022-03-30 09:20:26 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num < 0) {
+        return 'OOPS';
+    }
+    const fibSequence = [1, 1];
+    for (let i = 0; i < num; i++) {
+        fibSequence.push(fibSequence[i] + fibSequence[i + 1]);
+    }
+    return fibSequence[num - 1];
 };
 
 // Do not edit below this line
